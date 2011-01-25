861102e72d49452ba23968abe3bda60bf6311e93
Andre Duffeck <aduffeck@suse.de>
2011-01-25 13:15:54 +0100

    very basic (but functional) version

--- lib/has_mobile_views.rb
+++ lib/has_mobile_views.rb
@@ -1,1 +1,39 @@
-# HasMobileViews
+module ActionController
+  module HasMobileViews
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def has_mobile_views opts = {}
+        class_eval do
+          send :include, InstanceMethods
+
+          helper_method :mobile_request?
+          helper_method :mobile_browser?
+
+          before_filter :prepend_view_path_if_mobile
+        end
+      end
+    end
+
+    module InstanceMethods
+      def prepend_view_path_if_mobile
+        if mobile_request?
+          prepend_view_path 'app/mobile_views'
+        end
+      end
+
+      def mobile_request?
+        session[:mobile_view] = mobile_browser? if session[:mobile_view].nil?
+        session[:mobile_view]
+      end
+
+      def mobile_browser?
+        request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod|iPad|Android)/]
+      end
+    end
+  end
+end
+
+ActionController::Base.send(:include, ActionController::HasMobileViews)
---
+++ rails/init.rb
@@ -0,0 +1,1 @@
+require 'has_mobile_views'
