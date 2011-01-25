84950cd078b3e86fa718cfd659fa529c30f0c298
Thomas Schmidt <tschmidt@suse.de>
2011-01-25 14:20:10 +0100

    use params[:force_view] to set output format

--- lib/has_mobile_views.rb
+++ lib/has_mobile_views.rb
@@ -25,12 +25,13 @@ module ActionController
       end
 
       def mobile_request?
-        if params[:force_mobile_view].present?
+        session[:mobile_view] = mobile_browser? if session[:mobile_view].nil?
+        if params[:force_view] == 'mobile' && !session[:mobile_view]
+          logger.debug "Switching session to mobile view output"
           session[:mobile_view] = true
-        elsif params[:force_normal_view].present?
-          session[:normal_view] = false
-        else
-          session[:mobile_view] = mobile_browser? if session[:mobile_view].nil?
+        elsif params[:force_view] == 'normal' && session[:mobile_view]
+          logger.debug "Switching session to normal view output"
+          session[:mobile_view] = false
         end
         session[:mobile_view]
       end
